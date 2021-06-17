0ba64ee0352884074bff0183a60e3792a28af653
patel82 <chiragpatel1982.cp@gmail.com>
2021-06-17 11:30:03 +0100

    1

--- src/test/java/YNAP/com/RunCukesTest.java
+++ src/test/java/YNAP/com/RunCukesTest.java
@@ -5,15 +5,16 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources",tags = "@Superbrands",
+@CucumberOptions(features = "src\\test\\resources", tags = "@Superbrands",
         plugin = { "html:target/cucumber-html-report",
                 "json:target/cucumber-json-report.json",
                 "junit:target/cucumber-junit-report.xml",
                 "pretty:target/cucumber-pretty-report.txt"}
 
-)// feature bracket ends
+) //feature brecket ends
 
-class RunCukesTest {
+public class RunCukesTest {
 }
 
 
+//Local branch
